bbf335b1cd285b10db8c0ee0b20813c5ae0905b5
Saif0089 <67787465+Saif0089@users.noreply.github.com>
2024-04-24 17:26:50 +0500

    selection content prefab

---
+++ Assets/Scenes/JointToggleAnimations.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using DG.Tweening;
+using UnityEngine;
+using UnityEngine.UI;
+public class JointToggleAnimations : MonoBehaviour
+{
+    float InitialYScale;
+    float scaleDifference;
+    public float ExpandedYScale;
+
+    public RectTransform ParentToggleRect;
+
+    public List<GameObject> ToggleChilds = new List<GameObject>();
+    public RectTransform Content;
+
+    private void Start()
+    {
+        InitialYScale = ParentToggleRect.rect.height;
+        scaleDifference = ExpandedYScale - InitialYScale;
+    }
+
+    public void DoExpand(bool value)
+    {
+        if (value)
+        {
+            ParentToggleRect.DOSizeDelta(new Vector2(ParentToggleRect.rect.width, ExpandedYScale), 1).OnComplete(() =>
+            ToggleChilds.ForEach(x =>
+            {
+                x.SetActive(true);
+                x.GetComponent<Toggle>().isOn = true;
+            }));
+
+            Content.sizeDelta += new Vector2(0, scaleDifference);
+        }
+        else
+        {
+            ParentToggleRect.DOSizeDelta(new Vector2(ParentToggleRect.rect.width, InitialYScale), 1);
+            ToggleChilds.ForEach(x =>
+            {
+                x.SetActive(false);
+                x.GetComponent<Toggle>().isOn = false;
+            });
+            Content.sizeDelta -= new Vector2(0, scaleDifference);
+        }
+    }
+}
