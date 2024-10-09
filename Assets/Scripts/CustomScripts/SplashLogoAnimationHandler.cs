ae1782aa88cc5177cd24c364906fcfd739278c83
Saif0089 <67787465+Saif0089@users.noreply.github.com>
2024-10-09 17:54:10 +0500

    updated splash screen

--- Assets/Scripts/CustomScripts/SplashLogoAnimationHandler.cs
+++ Assets/Scripts/CustomScripts/SplashLogoAnimationHandler.cs
@@ -12,7 +12,7 @@ public class SplashLogoAnimationHandler : MonoBehaviour
     void Start()
     {
         Logo.DOFade(1, 3);
-        Logo.transform.DOScale(1.5f, 3).OnComplete(()=>
+        Logo.transform.DOScale(1f, 3).OnComplete(()=>
         {
             StartScene();
         });
